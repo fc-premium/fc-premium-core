b909e8968d185b400a7bc7afb549440ca10d1abf
pytness <pytness@gmail.com>
2020-03-18 11:45:40 +0100

    Commented all debug checks

--- src/module/debug/debug.ts
+++ src/module/debug/debug.ts
@@ -29,52 +29,52 @@ export class Debug {
 	}
 
 	log(...args: any[]): void {
-		if (this.hostModule.config.get('DEBUG_MODE') !== true)
-			return void 0;
+		// if (this.hostModule.config.get('DEBUG_MODE') !== true)
+		// 	return;
 
-		if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
-			args = [`[${this.hostModule.name}]`, ...args];
+		// if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
+		// 	args = [`[${this.hostModule.name}]`, ...args];
 
 		console.log(...args);
 	}
 
 	info(...args: any[]): void {
-		if (this.hostModule.config.get('DEBUG_MODE') !== true)
-			return void 0;
+		// if (this.hostModule.config.get('DEBUG_MODE') !== true)
+		// 	return;
 
-		if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
-			args = [`[${this.hostModule.name}]`, ...args];
+		// if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
+		// 	args = [`[${this.hostModule.name}]`, ...args];
 
 		console.info(...args);
 	}
 
 	warn(...args: any[]): void {
-		if (this.hostModule.config.get('DEBUG_MODE') !== true)
-			return void 0;
+		// if (this.hostModule.config.get('DEBUG_MODE') !== true)
+		// 	return;
 
-		if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
-			args = [`[${this.hostModule.name}]`, ...args];
+		// if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
+		// 	args = [`[${this.hostModule.name}]`, ...args];
 
 		console.warn(...args);
 
 	}
 
 	error(...args: any[]): void {
-		if (this.hostModule.config.get('DEBUG_MODE') !== true)
-			return void 0;
+		// if (this.hostModule.config.get('DEBUG_MODE') !== true)
+		// 	return;
 
-		if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
-			args = [`[${this.hostModule.name}]`, ...args];
+		// if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
+		// 	args = [`[${this.hostModule.name}]`, ...args];
 
 		console.error(...args);
 	}
 
 	alert(arg: any): void {
-		if (this.hostModule.config.get('DEBUG_MODE') !== true)
-			return void 0;
+		// if (this.hostModule.config.get('DEBUG_MODE') !== true)
+		// 	return;
 
-		if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
-			arg = `[${this.hostModule.name}] ${arg}`;
+		// if (this.hostModule.config.get('DEBUG_MODULE_NAME') === true)
+		// 	arg = `[${this.hostModule.name}] ${arg}`;
 
 		alert(arg);
 	}
