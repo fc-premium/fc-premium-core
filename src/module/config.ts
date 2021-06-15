25305f9b560bf800bf6f93d10787e3fc304d400c
Fernando Vega Catalán <fernando.vega@quironsalud.es>
2021-06-15 17:27:33 +0200

    Minor refactor

--- src/module/config.ts
+++ src/module/config.ts
@@ -1,5 +1,8 @@
 import { Module } from '../module'
 import { Core } from "../core";
+import { ConfigHandler } from '../config-handler';
+
+const ConfigHandler = Core.ConfigHandler;
 
 export class Config {
 
@@ -9,66 +12,66 @@ export class Config {
 		this.parentModule = module;
 	}
 
-	private getPrefixedKey(key: string): string {
+	private __add_key_prefix(key: string): string {
 		return `${this.parentModule.name}.${key}`;
 	}
 
 	public has(key: string): boolean {
-		key = this.getPrefixedKey(key);
-		return Core.ConfigHandler.has(key)
+		key = this.__add_key_prefix(key);
+		return ConfigHandler.has(key)
 	}
 
-	public getMeta(key: string): any {
-		key = this.getPrefixedKey(key);
-
-		if (!Core.ConfigHandler.has(key))
-			throw new Error(`'${key}' does not exist`)
-
-		return Core.ConfigHandler.getMeta(key);
-	}
-
-	public get(key: string): any {
-		key = this.getPrefixedKey(key);
-
-		if (!Core.ConfigHandler.has(key))
-			throw new Error(`'${key}' does not exist`)
-
-		return Core.ConfigHandler.get(key);
-	}
-
-	public set(key: string, value: any): void {
-		key = this.getPrefixedKey(key);
-
-		if (!Core.ConfigHandler.has(key))
-			throw new Error(`'${key}' does not exist`)
-
-		Core.ConfigHandler.set(key, value);
-	}
-
-	public keys(fullpath: boolean = false): string[] {
+	public keys(add_prefix: boolean = false): string[] {
 		const prefix = `${this.parentModule.name}.`;
 
-		let keys: string[] = Core.ConfigHandler.keys().filter((key) =>
+		let keys: string[] = ConfigHandler.keys().filter((key) =>
 			key.startsWith(prefix)
 		);
 
-		if (fullpath === false)
+		if (add_prefix === false)
 			keys = keys.map(key => key.slice(prefix.length))
 
 		return keys;
 	}
 
+	public getMeta(key: string): ConfigHandler.Setting {
+		key = this.__add_key_prefix(key);
+
+		if (!ConfigHandler.has(key))
+			throw new Error(`'${key}' does not exist`)
+
+		return ConfigHandler.getMeta(key);
+	}
+
+	public get(key: string): any {
+		key = this.__add_key_prefix(key);
+
+		if (!ConfigHandler.has(key))
+			throw new Error(`'${key}' does not exist`)
+
+		return ConfigHandler.get(key);
+	}
+
+	public set(key: string, value: any): void {
+		key = this.__add_key_prefix(key);
+
+		if (!ConfigHandler.has(key))
+			throw new Error(`'${key}' does not exist`)
+
+		ConfigHandler.set(key, value);
+	}
+
 	public reset(key: string, autosave: boolean = true) {
-		key = this.getPrefixedKey(key);
-		Core.ConfigHandler.reset(key);
+		key = this.__add_key_prefix(key);
+		ConfigHandler.reset(key);
 	}
 
 	public resetAll() {
 		this.keys(true).forEach(key =>
-			Core.ConfigHandler.reset(key)
+			ConfigHandler.reset(key)
 		);
 	}
 }
 
-// Exporting as a namespace so  it can be imported inside another namespace
+// Exporting as a namespace so it can be imported inside another namespace
 export namespace Config { };
